071730976ad4508fa897c1bb2c8f1614ebaed2c2
BAHADIRking <b123qweasdzxc@gmail.com>
2021-05-09 22:31:20 +0300

    button animation added

--- python-gui-version/interface.py
+++ python-gui-version/interface.py
@@ -1,6 +1,6 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-
+### MADE BY BAHADIR54
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
